226ca2bac8d481a21208895e9854b4f749b37e3d
Orion Kindel <cakekindel@gmail.com>
2022-06-05 13:44:36 -0700

    holding no longer infinitely appends sounds

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,19 @@
 use gpio::{GpioIn, GpioValue};
 use patch::SoundKey;
 use rodio::OutputStream;
+use std::collections::HashMap;
 use std::thread::sleep;
 use std::time::Duration;
 
 mod patch;
+
+fn gpio_is_hi(lane: &u16) -> bool {
+    gpio::sysfs::SysFsGpioInput::open(*lane)
+        .unwrap_or_else(|_| panic!("gpio lane {} should exist", lane))
+        .read_value()
+        .unwrap_or_else(|_| panic!("gpio lane {} should be an input", lane))
+        == GpioValue::High
+}
 
 fn main() {
     std::env::set_var(
@@ -16,6 +25,8 @@ fn main() {
         "dog_button=info,symphonia_core=error,symphonia_bundle_mp3=error",
     );
     env_logger::init();
+
+    let mut gpio_was_hi = HashMap::<u16, bool>::new();
 
     let profile = patch::Patch::get();
     let (_stream, stream_handle) = OutputStream::try_default().expect("audio should be available");
@@ -26,19 +37,15 @@ fn main() {
         .map(|s| s.play(&stream_handle));
 
     loop {
-        profile.sounds.iter().for_each(|(key, sound)| match key {
-            SoundKey::Gpio(lane) => {
-                if gpio::sysfs::SysFsGpioInput::open(*lane)
-                    .unwrap_or_else(|_| panic!("gpio lane {} should exist", lane))
-                    .read_value()
-                    .unwrap_or_else(|_| panic!("gpio lane {} should be an input", lane))
-                    == GpioValue::High
-                {
+        profile.sounds.iter().for_each(|(key, sound)| {
+            if let SoundKey::Gpio(lane) = key {
+                if gpio_is_hi(lane) && !gpio_was_hi.get(lane).copied().unwrap_or_default() {
                     log::info!("lane {} high", lane);
                     sound.play(&stream_handle);
+                } else {
+                    gpio_was_hi.insert(*lane, false);
                 }
             }
-            _ => (),
         });
 
         sleep(Duration::from_millis(10));
--- src/patch.rs
+++ src/patch.rs
@@ -3,7 +3,6 @@ use std::{
     fs::File,
     io::{Cursor, Read},
     iter::once,
-    sync::Barrier,
     thread,
 };
 
